3abe9f03b38196a684b734093c2bcfe005d6b7d3
Barry G. Becker <barrybecker4@yahoo.com>
2016-07-23 15:41:51 -0700

    remove unused collider from foodbit.

--- Assets/Scripts/Environment/Foodbit.cs
+++ Assets/Scripts/Environment/Foodbit.cs
@@ -8,7 +8,6 @@ public class Foodbit : MonoBehaviour
 {
 	public static float foodbitHeight = 1.0F;
 	
-	Ether eth;
 	MeshRenderer mr;
 
 	public float energy;
@@ -16,19 +15,16 @@ public class Foodbit : MonoBehaviour
 	 void Start ()
 	 {
 		name = "Foodbit";
-		
-		eth = Ether.getInstance();
-
 		mr = GetComponent<MeshRenderer>();
 		mr.sharedMaterial = (Material)Resources.Load("Materials/Foodbit");
 
-		Collider co = GetComponent<SphereCollider>();
-		co.isTrigger = true;
+		//Collider co = GetComponent<SphereCollider>();
+		//co.isTrigger = true;
 	 }
 
 	public void destroy ()
 	 {
-		eth.removeFoodbit(this.gameObject);
+		Ether.getInstance().removeFoodbit(this.gameObject);
 		Destroy(gameObject);
 	}
 
