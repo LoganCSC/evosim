dd09c83288d4a4cfa46f5a1eb4d3dfc928ecd5b4
Barry G. Becker <barrybecker4@yahoo.com>
2016-09-04 08:47:14 -0700

    try to recover from git mistake

---
+++ Assets/Scripts/GUI/LoadChromosomeButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadChromosomeButton : MonoBehaviour
+{
+	public Chromosome c;
+	public UIElement parent;
+
+	Settings s;
+
+	void Start ()
+	{
+		s = Settings.getInstance();
+	}
+
+	public void OnClick ()
+	{
+		Ether eth = Ether.getInstance();
+		eth.spawner.spawn(
+			Camera.main.transform.position + new Vector3(0, 0, 10),
+			Utility.RandomRotVec(),
+			s.init_energy,
+			c
+		);
+		Ether.getInstance().subtractEnergy(s.init_energy);
+		parent.make_invisible();
+		GetComponentInParent<CreatureList>().DepopulateMenu();
+	}
+}
